f8d7214dbb105da5e4cc25f9707bd933d08098e3
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-29 17:12:47 -0300

    Implementação do Heroes Service

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from "./hero.model";
 
 export const HEROES: Hero[] = [
-  { id : 1,       name: 'Wolverine'},
+  { id : 1, name: 'Wolverine'},
   { id : 2, name: 'Homem Aranha'},
   { id : 3, name: 'Batman'},
   { id : 4, name: 'Mulher Maravilha'},
